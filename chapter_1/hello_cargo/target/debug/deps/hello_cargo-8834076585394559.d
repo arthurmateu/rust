b201f353967ab4932ce88d7186a4d1ee8cf98317
Arthur Mateu <arthurmateu@gmail.com>
2024-06-18 01:25:12 +0200

    Hello, Cargo!

---
+++ chapter_1/hello_cargo/target/debug/deps/hello_cargo-8834076585394559.d
@@ -0,0 +1,5 @@
+/home/arthur/Projects/learning/rust/chapter_1/hello_cargo/target/debug/deps/libhello_cargo-8834076585394559.rmeta: src/main.rs
+
+/home/arthur/Projects/learning/rust/chapter_1/hello_cargo/target/debug/deps/hello_cargo-8834076585394559.d: src/main.rs
+
+src/main.rs:
---
+++ chapter_1/hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/home/arthur/Projects/learning/rust/chapter_1/hello_cargo/target/debug/hello_cargo: /home/arthur/Projects/learning/rust/chapter_1/hello_cargo/src/main.rs

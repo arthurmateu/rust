dd7ff4701fbba161cb0bd309724dd9e104f6ba69
Arthur Mateu <arthurmateu@gmail.com>
2024-06-27 01:00:17 +0200

    Mod and Filesystem

---
+++ chapter_7/communicator/target/debug/deps/communicator-741d7ff040ff022f.d
@@ -0,0 +1,6 @@
+/home/arthur/Projects/learning/rust/chapter_7/communicator/target/debug/deps/libcommunicator-741d7ff040ff022f.rmeta: src/lib.rs src/client.rs
+
+/home/arthur/Projects/learning/rust/chapter_7/communicator/target/debug/deps/communicator-741d7ff040ff022f.d: src/lib.rs src/client.rs
+
+src/lib.rs:
+src/client.rs:
---
+++ chapter_7/communicator/target/debug/deps/communicator-b744b3afb85af121.d
@@ -0,0 +1,6 @@
+/home/arthur/Projects/learning/rust/chapter_7/communicator/target/debug/deps/libcommunicator-b744b3afb85af121.rmeta: src/lib.rs src/client.rs
+
+/home/arthur/Projects/learning/rust/chapter_7/communicator/target/debug/deps/communicator-b744b3afb85af121.d: src/lib.rs src/client.rs
+
+src/lib.rs:
+src/client.rs:

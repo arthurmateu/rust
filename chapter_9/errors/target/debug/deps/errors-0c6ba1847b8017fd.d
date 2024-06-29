d93121c3cce33fc9e7043943ca8868f4ba860067
Arthur Mateu <arthurmateu@gmail.com>
2024-06-29 21:56:57 +0200

    Errors

---
+++ chapter_9/errors/target/debug/deps/errors-0c6ba1847b8017fd.d
@@ -0,0 +1,5 @@
+/home/arthur/Projects/learning/rust/chapter_9/errors/target/debug/deps/liberrors-0c6ba1847b8017fd.rmeta: src/main.rs
+
+/home/arthur/Projects/learning/rust/chapter_9/errors/target/debug/deps/errors-0c6ba1847b8017fd.d: src/main.rs
+
+src/main.rs:
---
+++ chapter_9/errors/target/debug/errors.d
@@ -0,0 +1,1 @@
+/home/arthur/Projects/learning/rust/chapter_9/errors/target/debug/errors: /home/arthur/Projects/learning/rust/chapter_9/errors/src/main.rs
